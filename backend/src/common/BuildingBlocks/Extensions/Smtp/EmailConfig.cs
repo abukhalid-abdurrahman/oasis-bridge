644c1e8003e3c130f3a3df0e087a57988f3ce365
nazarovqurbonali <nazarovqurbonali4@gmail.com>
2025-04-11 11:27:05 +0500

    refactoring email-config

--- backend/src/common/BuildingBlocks/Extensions/Smtp/EmailConfig.cs
+++ backend/src/common/BuildingBlocks/Extensions/Smtp/EmailConfig.cs
@@ -5,10 +5,10 @@ public sealed class EmailConfig
     public required string SmtpServer { get; init; }
     public required int SmtpPort { get; init; }
     public required string SenderEmailAddress { get; init; }
-    public required string SenderName { get; init; } = "OASIS";
+    public required string SenderName { get; init; } 
     public required string AppPassword { get; init; }
     public bool EnableSsl { get; init; } = true;
-    public int Timeout { get; init; } = 10000;
-    public int MaxRetryAttempts { get; init; } = 3;
-    public int RetryDelay { get; init; } = 3000;
+    public int Timeout { get; init; } 
+    public int MaxRetryAttempts { get; init; } 
+    public int RetryDelay { get; init; }
 }
