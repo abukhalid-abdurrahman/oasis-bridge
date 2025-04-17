e2fb1c84530775cf672349cb248b994f7662aa9e
nazarovqurbonali <nazarovqurbonali4@gmail.com>
2025-04-18 02:42:42 +0500

    refactoring

--- backend/src/api/API/Infrastructure/DI/CustomServiceRegister.cs
+++ backend/src/api/API/Infrastructure/DI/CustomServiceRegister.cs
@@ -26,16 +26,10 @@ public static class CustomServiceRegister
 
         // Registering order-related services.
         builder.Services.AddScoped<IOrderService, OrderService>();
-
-        // Register ipfs-services
-        builder.Services.AddScoped<IIpfsService, IpfsService>();
-
+        
         // Registering network-related services.
         builder.Services.AddScoped<INetworkService, NetworkService>();
-        
-        // Register image-optimizer services
-        builder.Services.AddScoped<IImageOptimizer, ImageOptimizer>();
-
+    
         // Registering identity-related services (authentication, registration, etc.).
         builder.Services.AddScoped<IIdentityService, IdentityService>();
 
