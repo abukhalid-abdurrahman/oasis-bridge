95a2f7a8bc08a9925e957efb45d4840061571b97
nazarovqurbonali <nazarovqurbonali4@gmail.com>
2025-04-27 22:10:38 +0500

    refactoring

--- backend/src/api/Infrastructure/ImplementationContract/Nft/NftMetadataSerializer.cs
+++ backend/src/api/Infrastructure/ImplementationContract/Nft/NftMetadataSerializer.cs
@@ -1,41 +1,41 @@
 using System.Text.Json;
 using Common.Contracts.Nft;
 
-namespace Infrastructure.ImplementationContract.Nft;
+using Infrastructure.Extensions.Json;
 
-public sealed class NftMetadataSerializer(
-    IDecentralizedFileStorage fileStorage,
-    ILogger<NftMetadataSerializer> logger,
-    IOptionsMonitor<IpfsOptions> options) : INftMetadataSerializer
+namespace Infrastructure.ImplementationContract.Nft
 {
-    public async Task<Result<string>> SerializeAsync(Common.DTOs.Nft nft, CancellationToken token = default)
+    public sealed class NftMetadataSerializer(
+        IDecentralizedFileStorage fileStorage,
+        ILogger<NftMetadataSerializer> logger,
+        IOptionsMonitor<IpfsOptions> options) : INftMetadataSerializer
     {
-        DateTimeOffset date = DateTimeOffset.UtcNow;
-        logger.OperationStarted(nameof(SerializeAsync), date);
-
-        try
+        public async Task<Result<string>> SerializeAsync(Common.DTOs.Nft nft, CancellationToken token = default)
         {
-            string json = JsonSerializer.Serialize(nft, new JsonSerializerOptions
+            DateTimeOffset date = DateTimeOffset.UtcNow;
+            logger.OperationStarted(nameof(SerializeAsync), date);
+
+            try
             {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                WriteIndented = true
-            });
+                using MemoryStream memoryStream = new();
+                await JsonSerializer.SerializeAsync(memoryStream, nft, NftMetadataSerializerContext.Default.Nft, token);
 
-            byte[] byteArray = Encoding.UTF8.GetBytes(json);
-            using MemoryStream memoryStream = new(byteArray);
+                memoryStream.Seek(0, SeekOrigin.Begin);  
 
-            string uniqueFileName = $"{nft.Name}_{Guid.NewGuid():N}_metadata.json";
-            string cid = await fileStorage.CreateAsync(memoryStream, uniqueFileName, token);
-            string fileUrl = options.CurrentValue.GatewayUrl + cid;
+                string uniqueFileName = $"{nft.Name}_{Guid.NewGuid():N}_metadata.json";
+                
+                string cid = await fileStorage.CreateAsync(memoryStream, uniqueFileName, token);
+                string fileUrl = options.CurrentValue.GatewayUrl + cid;
 
-            logger.OperationCompleted(nameof(SerializeAsync), DateTimeOffset.UtcNow, DateTimeOffset.UtcNow - date);
-            return Result<string>.Success(fileUrl);
-        }
-        catch (Exception ex)
-        {
-            logger.OperationException(nameof(SerializeAsync), ex.Message);
-            logger.OperationCompleted(nameof(SerializeAsync), DateTimeOffset.UtcNow, DateTimeOffset.UtcNow - date);
-            return Result<string>.Failure(ResultPatternError.InternalServerError(ex.Message));
+                logger.OperationCompleted(nameof(SerializeAsync), DateTimeOffset.UtcNow, DateTimeOffset.UtcNow - date);
+                return Result<string>.Success(fileUrl);
+            }
+            catch (Exception ex)
+            {
+                logger.OperationException(nameof(SerializeAsync), ex.Message);
+                logger.OperationCompleted(nameof(SerializeAsync), DateTimeOffset.UtcNow, DateTimeOffset.UtcNow - date);
+                return Result<string>.Failure(ResultPatternError.InternalServerError(ex.Message));
+            }
         }
     }
 }
