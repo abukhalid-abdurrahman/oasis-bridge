a3d731ccd68ef629166b3dac47e8f27a4e35f810
unknown <jofie.fir@gmail.com>
2025-05-14 22:42:20 +0500

    fix: Fixed param type in the page file

--- frontend/src/app/nft/[id]/update/page.tsx
+++ frontend/src/app/nft/[id]/update/page.tsx
@@ -1,4 +1,4 @@
-'use server'
+"use server";
 
 import Header from "@/components/Header";
 import { searchParams } from "@/lib/types";
@@ -6,13 +6,13 @@ import ChangeNft from "./components/ChangeNFT";
 
 export default async function page({
   searchParams,
-  params
+  params,
 }: {
   searchParams: Promise<searchParams>;
-  params: { id: string };
+  params: any;
 }) {
   return (
-    <div className='max-w-[1200px] mx-auto'>
+    <div className="max-w-[1200px] mx-auto">
       <Header searchParams={searchParams} />
       <div className="mt-24 xl:px-5 md:mt-14 md:!px-0 sm:!mt-10">
         <div className="mx-auto">
