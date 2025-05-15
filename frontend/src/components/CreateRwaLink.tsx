31f38f7e77237f8ddfe8930307fc16f680bf2561
unknown <jofie.fir@gmail.com>
2025-05-15 20:02:22 +0500

    fix: Fixed showing Create rwa to unauthorized users

--- frontend/src/components/CreateRwaLink.tsx
+++ frontend/src/components/CreateRwaLink.tsx
@@ -2,11 +2,10 @@
 
 import { useUserStore } from '@/store/useUserStore'
 import Link from 'next/link'
-import React from 'react'
 
 export default function CreateRwaLink() {
   const { user } = useUserStore()
-  if (user) {
-    return <Link href={`${user ? "/rwa/create" : "?signin=true"}`}>Create RWA</Link>
-  }
+  return (
+    <Link href={`${user ? "/rwa/create" : "?signin=true"}`}>Create RWA</Link>
+  )
 }
