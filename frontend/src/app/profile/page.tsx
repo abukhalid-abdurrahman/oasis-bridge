b55db4fdfac119e36b627bec559617ca0f4f23ea
unknown <jofie.fir@gmail.com>
2025-05-15 18:19:07 +0500

    design: added padding-bottom to profile page

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -14,7 +14,7 @@ export default async function page({
   return (
     <div className="">
       <Header searchParams={searchParams} />
-      <div className="max-w-[512px] mx-auto mt-20 md:mt-14">
+      <div className="max-w-[512px] mx-auto mt-20 md:mt-14 md:pb-5 sm:!mt-10 xs:!mt-7">
         <AccountAddresses />
         <LinkedWallets />
         <ChangePasswordForm />
