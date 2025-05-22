d27c36700b4306f42c3b792dac4c6b8f41885b60
unknown <jofie.fir@gmail.com>
2025-05-22 16:38:01 +0500

    feature: CHanged max file size to 100mb

--- frontend/src/lib/constants/index.ts
+++ frontend/src/lib/constants/index.ts
@@ -1,6 +1,6 @@
 export const API = process.env.NEXT_PUBLIC_API_URL;
 export const SOLANA_NET = process.env.NEXT_PUBLIC_SOLANA_NET;
-export const MAX_FILE_SIZE = 10 * 1024 * 1024;
+export const MAX_FILE_SIZE = 100 * 1024 * 1024;
 export const ACCEPTED_DOCUMENT_TYPES = [
   "image/jpeg",
   "image/jpg",
